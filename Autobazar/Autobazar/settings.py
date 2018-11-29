adfd3548b79f9de0fee9727eb0bc66ae2bca3327
Antonín Mrkvica <antonin.mrkvica@outlook.cz>
2018-11-29 20:03:56 +0100

    create index page

--- Autobazar/Autobazar/settings.py
+++ Autobazar/Autobazar/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Autobazar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["./polls/temp"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Autobazar/polls/urls.py
+++ Autobazar/polls/urls.py
@@ -1,1 +1,7 @@
-__author__ = 'Antonín Mrkvica'
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
