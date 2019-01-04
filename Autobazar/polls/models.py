9ad605877ae11a5c175592d3b3bded71a8a3a702
Antonín Mrkvica <antonin.mrkvica@outlook.cz>
2019-01-04 11:16:52 +0100

    create User model

--- Autobazar/polls/models.py
+++ Autobazar/polls/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    is_admin = models.BooleanField(default=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
