2445eed21ca7847241ed9f5e6b3ab641c3fbb072
Antonín Mrkvica <antonin.mrkvica@outlook.cz>
2019-01-09 19:27:04 +0100

    sell page content add

--- Autobazar/polls/views.py
+++ Autobazar/polls/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
-from .models import User
+from .models import User, Car
+
 
 # Create your views here.
 
-def index(request):
-    if (request.session.get('user_id')):
-        logged = True
+def logged(req):
+    if (req.session.get('user_id')):
+        return True
     else:
-        logged = False
-    return render(request, "./index.html", {"logged": logged})
+        return False
+
+
+def index(request):
+    return render(request, "./index.html", {"logged": logged(request)})
 
 
 def login(request):
@@ -48,3 +52,29 @@ def reg(request):
 def logout(request):
     del request.session['user_id']
     return redirect("index")
+
+
+def buy(request):
+    return render(request, "./buy.html", {"logged": logged(request)})
+
+
+def sell(request):
+    if request.method == "POST":
+        return redirect("index")
+    return render(request, "./sell.html",
+                  {"logged": logged(request),
+                   "car_param": [["znacka", "Značka"], ["model", "Model"],
+                                 ["motorizace", "Motorizace"], ["tachometr", "Tachometr"],
+                                 ["cena", "Cena"], ["datum_vyroby", "Datum výroby"],
+                                 ["vyhrivana_sedadla", "Vyhřívaná sedadla"],
+                                 ["airbag", "Airbag"],
+                                 ["mlhovky", "Mlhovky"],
+                                 ["denni_sviceni", "Denní svícení"],
+                                 ["rezervni_kolo", "Rezervní kolo"],
+                                 ["vyhrivane_celni_sklo", "Vyhřívané čelní sklo"],
+                                 ["vyhrivana_zrcatka", "Vyhřívaná zrcátka"],
+                                 ["delena_zadni_sedadla", "Dělená zadní sedadla"],
+                                 ["klimatizace", "Klimatizace"]]})
+
+def search(request):
+    return render(request, "./search.html", {"logged": logged(request)})
