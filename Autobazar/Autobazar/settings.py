db22ac1d31963fa0966d5678fff97e99483a3a50
Antonín Mrkvica <antonin.mrkvica@outlook.cz>
2019-04-06 12:43:21 +0200

    debug false

--- Autobazar/Autobazar/settings.py
+++ Autobazar/Autobazar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rqv40=_0%@p##9=+7+hj9^p+8pa*e=#=*24gx=87ml1wh3gx1!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
